ab9468130ca27d1111deb8ca93a32cb23b8ae4c3
Niek <niekvanmuylwijck@gmail.com>
2020-10-08 13:59:08 +0200

    Maze scale removed. MazeBuilt event added

--- Assets/Scripts/MazeBuilder.cs
+++ Assets/Scripts/MazeBuilder.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Scripts
@@ -8,7 +9,6 @@ namespace Assets.Scripts
         [Header("Maze settings")]
         [SerializeField, Min(1)] private int _mazeWidth = 10;
         [SerializeField, Min(1)] private int _mazeHeight = 10;
-        [SerializeField, Min(.1f)] private float _mazeScale = 1f;
         [Header("Maze Object Components")]
         [SerializeField] private GameObject _wallObject;
         [SerializeField] private GameObject _floorObject;
@@ -16,6 +16,8 @@ namespace Assets.Scripts
         private Maze _maze;
         private List<GameObject> _walls;
         private List<GameObject> _floors;
+
+        public static event Action<Maze> MazeBuilt;
 
         void Start()
         {
@@ -43,25 +45,29 @@ namespace Assets.Scripts
                 int column = i % _mazeWidth + 1;
                 
                 // get the current cell's position
-                Vector3 cellPos = new Vector3(-(column * _mazeScale), 0 ,row * _mazeScale);
+                Vector3 cellPos = new Vector3(-column, 0 ,row);
                 // instantiate floor object at cell position and add to _floors list
                 _floors.Add(Instantiate(_floorObject, cellPos,Quaternion.identity));//TODO: instantiate floor as one object under entire maze
 
                 // instantiate a wall if one should be present. offset by half the cell size and rotate depending on which side it is.
                 // walls get added to the _walls list
-                if (c.Down) _walls.Add(Instantiate(_wallObject, cellPos - Vector3.back * _mazeScale / 2 + _wallObject.transform.position, _wallObject.transform.rotation ));
-                if (c.Left) _walls.Add(Instantiate(_wallObject, cellPos - Vector3.left * _mazeScale / 2 + _wallObject.transform.position, _wallObject.transform.rotation * Quaternion.Euler(0, 90, 0)));
-                if (c.Up) _walls.Add(Instantiate(_wallObject, cellPos - Vector3.forward * _mazeScale / 2 + _wallObject.transform.position, _wallObject.transform.rotation * Quaternion.Euler(0,180, 0)));
-                if (c.Right) _walls.Add(Instantiate(_wallObject, cellPos - Vector3.right * _mazeScale / 2 + _wallObject.transform.position, _wallObject.transform.rotation * Quaternion.Euler(0, 270, 0)));
+                if (c.Down) _walls.Add(Instantiate(_wallObject, cellPos - Vector3.back / 2 + _wallObject.transform.position, _wallObject.transform.rotation ));
+                if (c.Left) _walls.Add(Instantiate(_wallObject, cellPos - Vector3.left / 2 + _wallObject.transform.position, _wallObject.transform.rotation * Quaternion.Euler(0, 90, 0)));
+                if (c.Up) _walls.Add(Instantiate(_wallObject, cellPos - Vector3.forward / 2 + _wallObject.transform.position, _wallObject.transform.rotation * Quaternion.Euler(0,180, 0)));
+                if (c.Right) _walls.Add(Instantiate(_wallObject, cellPos - Vector3.right / 2 + _wallObject.transform.position, _wallObject.transform.rotation * Quaternion.Euler(0, 270, 0)));
             }
 
-            _walls.ForEach(w => w.transform.localScale *= _mazeScale);
-            _floors.ForEach(f => f.transform.localScale *= _mazeScale);
+            OnMazeBuilt(_maze);
         }
 
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.Space)) BuildMaze();
+        }
+
+        private static void OnMazeBuilt(Maze maze)
+        {
+            MazeBuilt?.Invoke(maze);
         }
     }
 }
