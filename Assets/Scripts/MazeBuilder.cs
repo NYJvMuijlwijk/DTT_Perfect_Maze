3ed4b5b43a63f79cbf6a7f841fadadca96114af6
Niek <niekvanmuylwijck@gmail.com>
2020-10-08 12:14:22 +0200

    MazeBuilder script added with code for testing maze generation

---
+++ Assets/Scripts/MazeBuilder.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class MazeBuilder : MonoBehaviour
+    {
+        [SerializeField] private int _mazeWidth = 10;
+        [SerializeField] private int _mazeHeight = 10;
+
+        private Maze _maze;
+
+
+        public void BuildMaze()
+        {
+            _maze = Eller.GenerateMaze(_mazeWidth, _mazeHeight);
+            Debug.Log(_maze);
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) BuildMaze();
+        }
+    }
+}
