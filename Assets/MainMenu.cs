3839e7333bbe498f43d97daec9a1c7a7d37bab40
Niek <niekvanmuylwijck@gmail.com>
2020-10-10 19:39:30 +0200

    Main Menu added

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public static event Action PlayGame;
+        public static event Action MainMenuOpened;
+
+        public void Play()
+        {
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(false);
+            }
+
+            OnPlayGame();
+        }
+
+        public void OpenMainMenu()
+        {
+            foreach (Transform child in GetComponentsInChildren<Transform>(true))
+            {
+                child.gameObject.SetActive(true);
+            }
+
+            OnMainMenuOpened();
+        }
+
+        /// <summary>
+        /// Exit the program
+        /// </summary>
+        public void Quit()
+        {
+            Application.Quit();
+        }
+
+        private static void OnPlayGame()
+        {
+            PlayGame?.Invoke();
+        }
+
+        private static void OnMainMenuOpened()
+        {
+            MainMenuOpened?.Invoke();
+        }
+    }
+}
