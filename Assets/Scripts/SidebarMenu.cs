87e081dfd82f74fa4b0a59a5bcee85f399caa085
Niek <niekvanmuylwijck@gmail.com>
2020-10-10 18:01:21 +0200

    added methods for toggling sidebar visibility

--- Assets/Scripts/SidebarMenu.cs
+++ Assets/Scripts/SidebarMenu.cs
@@ -7,12 +7,14 @@ namespace Assets.Scripts
     public class SidebarMenu : MonoBehaviour
     {
         [SerializeField, Range(0,1)] private float _menuWidthPercent = .3f;
-        [SerializeField] private bool _visible = true;
+        [SerializeField] private bool _visible;
         [SerializeField] private RectTransform _toggleButton;
+        [SerializeField] private float _toggleSize = 60f;
         [SerializeField] private GameObject _sidebarMenu;
 
         private RectTransform _transform;
-        private float _canvasWidth;
+        private Vector2 _canvasDimensions;
+        private Image _toggleImage;
 
         void Awake()
         {
@@ -21,36 +23,68 @@ namespace Assets.Scripts
 
         void Start()
         {
-            _canvasWidth = GetComponent<CanvasScaler>().referenceResolution.x;
+            _canvasDimensions = GetComponent<CanvasScaler>().referenceResolution;
+            _toggleImage = _toggleButton.GetComponent<Image>();
 
+            // show/hide sidebar depending on visibility
             if (_visible)
-            {
-                _transform.offsetMin = Vector2.right * (_canvasWidth - _canvasWidth * _menuWidthPercent);
-                _transform.offsetMax = Vector2.zero;
-            }
+                ShowSidebar();
+            else 
+                HideSidebar();
         }
 
+        /// <summary>
+        /// (un)hides the sidebar menu
+        /// </summary>
         public void ToggleMenu()
         {
-            float buttonWidth = (_toggleButton.offsetMax - _toggleButton.offsetMin).x;
+            // depending on visibility, move sidebar and toggle to the side or to its specified width
             if (_visible)
-            {
-                _transform.offsetMin = Vector2.right * _canvasWidth;
-                _transform.offsetMax = Vector2.right * _canvasWidth * _menuWidthPercent;
-                //_toggleButton.transform.position = new Vector2(_canvasWidth / 2 - buttonWidth,_canvasWidth/2);
-                _sidebarMenu.SetActive(false);
-            }
+                HideSidebar();
             else
-            {
-                _sidebarMenu.SetActive(true);
-                _transform.offsetMin = Vector2.right * (_canvasWidth - _canvasWidth * _menuWidthPercent);
-                _transform.offsetMax = Vector2.zero;
-                //_toggleButton.transform.position = new Vector2(_canvasWidth / 2 - buttonWidth/2 - _canvasWidth * _menuWidthPercent, _canvasWidth / 2);
+                ShowSidebar();
+        }
 
-            }
+        /// <summary>
+        /// Sets the sidebars position and dimensions according to the menu width percentage
+        /// </summary>
+        private void ShowSidebar()
+        {
+            // enable the menu
+            _sidebarMenu.SetActive(true);
+            // move sidebar back to its specified width
+            _transform.offsetMin = Vector2.right * (_canvasDimensions.x - _canvasDimensions.x * _menuWidthPercent);
+            _transform.offsetMax = Vector2.zero;
+            // move toggle button to the edge of the menu
+            _toggleButton.offsetMin =
+                new Vector2(_canvasDimensions.x / 2 - _canvasDimensions.x * _menuWidthPercent - _toggleSize / 2, -_toggleSize / 2);
+            _toggleButton.offsetMax =
+                new Vector2(_canvasDimensions.x / 2 - _canvasDimensions.x * _menuWidthPercent + _toggleSize / 2, _toggleSize / 2);
 
-            _toggleButton.Rotate(Vector3.forward, 180f);
-            _visible = !_visible;
+            // set icon rotation
+            _toggleImage.transform.rotation = Quaternion.identity;
+
+            _visible = true;
+        }
+
+        /// <summary>
+        /// Hides the sidebar menu
+        /// </summary>
+        private void HideSidebar()
+        {
+            // hide sidebar off screen
+            _transform.offsetMin = Vector2.right * _canvasDimensions.x;
+            _transform.offsetMax = Vector2.right * _canvasDimensions.x * _menuWidthPercent;
+            // move toggle button to the side of the screen
+            _toggleButton.offsetMin = new Vector2(_canvasDimensions.x / 2 - _toggleSize, -_toggleSize / 2);
+            _toggleButton.offsetMax = new Vector2(_canvasDimensions.x / 2, _toggleSize / 2);
+            // disable the menu
+            _sidebarMenu.SetActive(false);
+
+            // set icon rotation
+            _toggleImage.transform.rotation = Quaternion.identity * Quaternion.Euler(0,0,180f);
+
+            _visible = false;
         }
     }
 }
